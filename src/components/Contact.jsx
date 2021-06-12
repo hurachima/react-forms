1878eb9d11feecdc3abe2d3c0a35cbd705e504f4
hurachima <62026617+hurachima@users.noreply.github.com>
2021-06-12 18:01:20 +0300

    beta version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 import styles from './Contact.module.css';
 
 const Contact = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: {submitCount,errors}
+    } = useForm();
+
+    const onSubmit = (data) => {
+        let v = JSON.stringify(data);
+        alert(v);
+    }
+
     return (
         <div className={styles.form__wrapper}>
                 <div className={styles.form__header}>
@@ -10,24 +22,28 @@ const Contact = () => {
             </div>
 
             <div className="form__content">
-                <form> 
+                <form onSubmit={handleSubmit(onSubmit)}> 
                 <div className={styles.form__user}>
                     <label htmlFor="" className="form-label">
                             Your Email
                         </label>
                         <input type="email" className="form__user-input" placeholder="example@email.com" 
+                        {...register('email',{required:true, pattern:/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/})}
                         
                         />
+                        {errors.email && <i>Не вірна електронна адреса</i>}
                     </div>
 
                     <div className={styles.form__user}>
                     <label htmlFor="" className="form-label">
                             Subject
                         </label>
-                        <select name="" id="">
-                            <option value="">1</option>
-                            <option value="">2</option>
+                        <select {...register('subjects',{required: true})}>
+                            <option value=""></option>
+                            <option value="Math">Math</option>
+                            <option value="IT">IT</option>
                         </select>
+                        {errors.subjects && <i>Виберіть варіант</i>}
                     </div>
 
 
@@ -35,13 +51,16 @@ const Contact = () => {
                     <label htmlFor="" className="form-label">
                             Message
                         </label>
-                        <textarea name=""></textarea>
+                        <textarea placeholder="Type your message here..."  {...register('message',{required:true, minLength: 10})}></textarea>
+                        {errors.message && <i>Поле повинно містити від 10 символів</i>}
                     </div>
 
                     <div className="form__user">
                         <button type="submit" className={styles.submitBtn}>Send</button>
                         <button className={styles.forgotBtn}>Cancel</button>
                     </div>
+
+                    <div className="form__user">Разів відправлено: {submitCount}</div>
 
                 </form>
             </div>
